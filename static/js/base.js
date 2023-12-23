66c16c7ba0913dd5727826b73b8eb9ec22574650
Riccardo Fabbian <fabb.riccardo@gmail.com>
2023-12-24 00:41:38 +0100

    Added whatsapp link button

--- static/js/base.js
+++ static/js/base.js
@@ -20,3 +20,24 @@ const linkAction = () => {
   navMenu.classList.remove('show-menu');
 }
 navLink.forEach(l => l.addEventListener('click', linkAction));
+
+const form = document.querySelector('form');
+if (form) {
+  const submitBtn = document.querySelector('#contact__form-submit');
+  submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let user = form.elements['firstname'].value;
+    let message = form.elements['message'].value;
+
+    if (user && message) {
+      let whatsappURL = `https://wa.me/393500020002?text=Salve%20sono%20${user}%20${message}`;
+      console.log('WhatsApp URL:', whatsappURL);
+
+      window.location.replace(whatsappURL);
+    } else {
+      console.error('Please fill in both the user and message fields.');
+    }
+  });
+} else {
+  console.error('Form or submit button not found.');
+}
